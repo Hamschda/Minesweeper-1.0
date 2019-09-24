7a26db5e35955b853cbab0dab8faace8d4fff07f
Hamschda <52716495+Hamschda@users.noreply.github.com>
2019-09-24 22:31:14 +0200

    Auslesen des Click-Events
    Auslesen des Tags uns aufspalten in Koordinaten + Aufdecken des Buttons

--- Minesweeper_V1.0/Form1.cs
+++ Minesweeper_V1.0/Form1.cs
@@ -11,20 +11,21 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        Button[,] btn = new Button[9, 9];
+        
         public Form1()
         {
             InitializeComponent();
-            Button[,] btn = new Button[9,9]; 
             int size = 17; //Buttongröße
             
-            //Erstellen und Konfigurieren Buttons
+            //Erstellen und Konfigurieren der Buttons
             for (int x = 0; x < 9; x++)
-            {
-                
+            { 
                 for (int y = 0; y < 9; y++)
                 {
                     btn[x, y] = new Button();
                     btn[x, y].Name = "Button"+ x + y;
+                    btn[x, y].Tag = x + "." + y;
                     btn[x, y].Size = new Size(size, size);
                     btn[x, y].Click += new EventHandler(this.button_Click);
                     btn[x, y].Location = new Point(x * (size - 1), y * (size - 1));
@@ -37,20 +38,22 @@ namespace WindowsFormsApplication1
                     this.Controls.Add(btn[x, y]);
                 }
             }
-            
         }
 
         void button_Click(object sender, EventArgs e)
         {
-           // throw new NotImplementedException();
-            MessageBox.Show(Convert.ToString(sender));
-            
-            
+            string curtentTag = (string)((Button)sender).Tag; //auslesen des Tags
+            string[] coord = curtentTag.Split('.'); //zerteilen des Tags in die Koordinaten
+            int x = Convert.ToInt32(coord[0]); 
+            int y = Convert.ToInt32(coord[1]);
+            //MessageBox.Show(x+ "," +y);
+            MessageBox.Show(Convert.ToString(uncover(x, y)));
         }
 
         public int uncover(int x, int y)
         {
             int value = 0;
+            btn[x, y].Image = Properties.Resources.uncover;
             return value;
         }
 
