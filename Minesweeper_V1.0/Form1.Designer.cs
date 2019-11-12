20d8adb5a837857a54958b1784a9ffbcd540cb7b
Hamschda <52716495+Hamschda@users.noreply.github.com>
2019-11-12 12:52:15 +0100

    Icons + Installationsdatei

--- Minesweeper_V1.0/Form1.Designer.cs
+++ Minesweeper_V1.0/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer4 = new System.Windows.Forms.Timer(this.components);
             this.GameButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -55,10 +56,10 @@
             // 
             this.GameButton.FlatAppearance.BorderSize = 0;
             this.GameButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.GameButton.Location = new System.Drawing.Point(0, 1);
-            this.GameButton.Margin = new System.Windows.Forms.Padding(2);
+            this.GameButton.Location = new System.Drawing.Point(0, 2);
+            this.GameButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GameButton.Name = "GameButton";
-            this.GameButton.Size = new System.Drawing.Size(52, 21);
+            this.GameButton.Size = new System.Drawing.Size(104, 40);
             this.GameButton.TabIndex = 1;
             this.GameButton.Text = "Game";
             this.GameButton.UseVisualStyleBackColor = true;
@@ -78,20 +79,20 @@
             this.panel1.Controls.Add(this.radioButton3);
             this.panel1.Controls.Add(this.radioButton2);
             this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Location = new System.Drawing.Point(13, 35);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(26, 67);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(142, 154);
+            this.panel1.Size = new System.Drawing.Size(284, 296);
             this.panel1.TabIndex = 0;
             this.panel1.Visible = false;
             // 
             // OK_Button
             // 
             this.OK_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.OK_Button.Location = new System.Drawing.Point(6, 126);
-            this.OK_Button.Margin = new System.Windows.Forms.Padding(2);
+            this.OK_Button.Location = new System.Drawing.Point(12, 242);
+            this.OK_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.OK_Button.Name = "OK_Button";
-            this.OK_Button.Size = new System.Drawing.Size(52, 22);
+            this.OK_Button.Size = new System.Drawing.Size(104, 42);
             this.OK_Button.TabIndex = 11;
             this.OK_Button.Text = "OK";
             this.OK_Button.UseVisualStyleBackColor = true;
@@ -99,38 +100,38 @@
             // 
             // MinesTextBox
             // 
-            this.MinesTextBox.Location = new System.Drawing.Point(104, 102);
-            this.MinesTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.MinesTextBox.Location = new System.Drawing.Point(208, 196);
+            this.MinesTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MinesTextBox.Name = "MinesTextBox";
-            this.MinesTextBox.Size = new System.Drawing.Size(27, 20);
+            this.MinesTextBox.Size = new System.Drawing.Size(50, 31);
             this.MinesTextBox.TabIndex = 10;
             this.MinesTextBox.Text = "10";
             // 
             // HeightTextBox
             // 
-            this.HeightTextBox.Location = new System.Drawing.Point(76, 102);
-            this.HeightTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.HeightTextBox.Location = new System.Drawing.Point(152, 196);
+            this.HeightTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HeightTextBox.Name = "HeightTextBox";
-            this.HeightTextBox.Size = new System.Drawing.Size(18, 20);
+            this.HeightTextBox.Size = new System.Drawing.Size(32, 31);
             this.HeightTextBox.TabIndex = 9;
             this.HeightTextBox.Text = "8";
             // 
             // WidthTextBox
             // 
-            this.WidthTextBox.Location = new System.Drawing.Point(36, 102);
-            this.WidthTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.WidthTextBox.Location = new System.Drawing.Point(72, 196);
+            this.WidthTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.WidthTextBox.Name = "WidthTextBox";
-            this.WidthTextBox.Size = new System.Drawing.Size(18, 20);
+            this.WidthTextBox.Size = new System.Drawing.Size(32, 31);
             this.WidthTextBox.TabIndex = 8;
             this.WidthTextBox.Text = "8";
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(6, 62);
-            this.radioButton4.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton4.Location = new System.Drawing.Point(12, 119);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(60, 17);
+            this.radioButton4.Size = new System.Drawing.Size(116, 29);
             this.radioButton4.TabIndex = 7;
             this.radioButton4.Text = "Custom";
             this.radioButton4.UseVisualStyleBackColor = true;
@@ -138,40 +139,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 83);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(36, 160);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(67, 25);
             this.label1.TabIndex = 6;
             this.label1.Text = "Width";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(94, 83);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(188, 160);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(70, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Mines";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 83);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(108, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(74, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Height";
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(6, 44);
-            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Location = new System.Drawing.Point(12, 85);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(55, 17);
+            this.radioButton3.Size = new System.Drawing.Size(105, 29);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.Text = "Expert";
             this.radioButton3.UseVisualStyleBackColor = true;
@@ -179,10 +180,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(6, 25);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Location = new System.Drawing.Point(12, 48);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.Size = new System.Drawing.Size(161, 29);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.Text = "Intermediate";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -190,22 +191,23 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(6, 7);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Location = new System.Drawing.Point(12, 13);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.Size = new System.Drawing.Size(129, 29);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.Text = "Beginner";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 293);
+            this.ClientSize = new System.Drawing.Size(656, 563);
             this.Controls.Add(this.GameButton);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Minesweeper FTAT4";
